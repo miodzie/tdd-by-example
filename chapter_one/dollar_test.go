1a52a592c235dda994f6a7581074cd24f09c96a3
miodzie <miodzie@protonmail.com>
2022-12-06 10:10:14 -0500

    Added TODO/Story comments

--- chapter_one/dollar_test.go
+++ chapter_one/dollar_test.go
@@ -5,6 +5,13 @@ import (
 	"testing"
 )
 
+// TODO/Stories from book: (~<story>~ indicates completed)
+// $5 + 10 CHF = $10 if rate is 2:1
+// ~$5 * 2 = $10~
+// Make "amount" private
+// Dollar side effects?
+// Money rounding?
+
 func TestMultiplication(t *testing.T) {
 	five := NewDollar(5)
 	five.times(2)
