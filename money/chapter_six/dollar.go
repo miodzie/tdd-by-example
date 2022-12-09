e318a8fe1e7b6faf66ff71cb17c4de5a430f7a6b
miodzie <miodzie@protonmail.com>
2022-12-08 19:45:17 -0500

    Chapter 6 complete. Not sure if I implemented an interface too early, but I didn't want to have to do NewDollar(5).Money for shared comparisons

---
+++ money/chapter_six/dollar.go
@@ -0,0 +1,37 @@
+package chapter_six
+
+type Money interface {
+	Amount() int
+}
+
+type money struct {
+	amount int
+}
+
+func (m money) Amount() int {
+	return m.amount
+}
+
+func (m money) times(multiplier int) Money {
+	return money{amount: m.amount * multiplier}
+}
+
+func (m money) equals(money Money) bool {
+	return m.amount == money.Amount()
+}
+
+type Dollar struct {
+	money
+}
+
+func NewDollar(amount int) Dollar {
+	return Dollar{money{amount: amount}}
+}
+
+type Franc struct {
+	money
+}
+
+func NewFranc(amount int) Franc {
+	return Franc{money{amount: amount}}
+}
